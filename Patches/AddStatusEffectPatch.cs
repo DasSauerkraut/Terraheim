3af6f6c5c9f0ef998d4ac28ff593755a59a5d8fd
louis1706 <louismonneyron5@yahoo.com>
2022-11-01 02:03:41 +0100

    Do not required a if & change version

--- Patches/AddStatusEffectPatch.cs
+++ Patches/AddStatusEffectPatch.cs
@@ -6,13 +6,7 @@ namespace Terraheim.Patches
     class AddStatusEffectPatch
     {
         [HarmonyPatch(typeof(SEMan), nameof(SEMan.AddStatusEffect), typeof(string), typeof(bool))]
-        public static bool Prefix(SEMan __instance, ref string name)
-        {
-            if (__instance.HaveStatusEffect("Waterproof") && name == "Wet")
-            {
-                return false;
-            }
-            return true;
-        }
+        public static bool Prefix(SEMan __instance, ref string name) => 
+            !__instance.HaveStatusEffect("Waterproof") || name != "Wet";
     }
 }
